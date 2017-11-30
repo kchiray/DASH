5c4798e3c93ba4a0bcc4d128fa4c61ac45667d5a
kchiray <kchiray@my.yorku.ca>
2017-11-29 22:08:04 -0500

    Speech Test

--- DashConsole/DashConsole/Form1.cs
+++ DashConsole/DashConsole/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
 
 namespace DashConsole
 {
     public partial class Form1 : Form
     {
+        SpeechSynthesizer speech = new SpeechSynthesizer();
         public Form1()
         {
+            speech.SelectVoiceByHints(VoiceGender.Male);
+            speech.Speak("Hello");    
             InitializeComponent();
         }
+        
     }
+    
 }
